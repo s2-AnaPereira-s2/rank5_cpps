340cc6957fb1de41e88dc064a94f00fc5ff77a64
Ana <ana.she.po@gmail.com>
2026-04-30 23:07:24 +0200

    finished ex01

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ana-pdos <ana-pdos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ana <ana@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 16:20:44 by ana-pdos          #+#    #+#             */
-/*   Updated: 2026/04/30 17:25:08 by ana-pdos         ###   ########.fr       */
+/*   Updated: 2026/04/30 22:40:59 by ana              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
         return (std::cerr << "Invalid number of argument" << std::endl, 1);
-    RPN_calc(argv[1]);
+    RPN rpn;
+    rpn.RPN_calc(argv[1]);
     return 0;
 }
--- cpp09/ex01/src/RPN.cpp
+++ cpp09/ex01/src/RPN.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ana-pdos <ana-pdos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ana <ana@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 00:00:00 by ana               #+#    #+#             */
-/*   Updated: 2026/04/30 18:07:23 by ana-pdos         ###   ########.fr       */
+/*   Updated: 2026/04/30 23:03:34 by ana              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,19 +31,54 @@ void RPN::RPN_calc(std::string expression)
 {
     for (int i = 0; expression[i]; i++)
     {
-        if (isdigit(expression[i]))
+        if (!isdigit(expression[i]))
         {
-            numbers.push(expression[i]);
+            std::cerr << "Error: invalid expression" << std::endl;
+            return;
+        }
+        if (isspace(expression[i]))
+        {
+            continue;
+        }
+        else if (isdigit(expression[i]))
+        {
+            numbers.push(expression[i] - '0');
         }
         else if (expression[i] == '+' || expression[i] == '-' || expression[i] == '/' || expression[i] == '*')
         {
             int n1 = numbers.top();
             numbers.pop();
             int n2 = numbers.top();
+            numbers.pop();
+            switch (expression[i])
+            {
+                case '+':
+                    numbers.push(n2 + n1);
+                    break;
+                case '-':
+                    numbers.push(n2 - n1);
+                    break;
+                case '*':
+                    numbers.push(n2 * n1);
+                    break;
+                case '/':
+                    numbers.push(n2 / n1);
+                    break;
+            }
             
+        }
+        else if (expression[i] != '+' || expression[i] != '-' || expression[i] != '/' || expression[i] != '*')
+        {
+            std::cerr << "Error: invalid expression" << std::endl;
+            return;
         }
         
     }
-    
+    if (numbers.size() != 1)
+    {
+        std::cerr << "Error: invalid expression" << std::endl;
+        return;
+    }
+    std::cout << numbers.top() << std::endl;
 }
 
