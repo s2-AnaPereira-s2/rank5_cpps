b419c1a3b49f2168d3ce61cb1dd3bbb5a55fbe2c
Ana Paula Dos Santos Pereira <ana-pdos@c4b2c6.42berlin.de>
2026-04-30 11:39:03 +0200

    fast

--- cpp09/ex00/src/BitcoinShow.cpp
+++ cpp09/ex00/src/BitcoinShow.cpp
@@ -3,43 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinShow.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ana <ana@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: ana-pdos <ana-pdos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 00:00:00 by ana               #+#    #+#             */
-/*   Updated: 2026/04/29 22:12:24 by ana              ###   ########.fr       */
+/*   Updated: 2026/04/30 11:38:29 by ana-pdos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int date_check(std::string date)
+static int date_check(std::string date)
 {
-    int sc = 0;
-    for (int i = 0; date[i]; i++)
-    {
-        if (date[i] == '-')
-            sc++;
-    }
-    if (sc != 2)
+    if (date.length() != 10)
         return 0;
-    int i = 0;
-    for (; date[i]; i++);
-    if (i != 10)
+    int year, month, day;
+    if (sscanf(date.c_str(), "%4d-%2d-%2d", &year, &month, &day) != 3)
         return 0;
-    std::string year;
-    size_t pos = date.find('-');
-    
+    if (month < 1 || month > 12 || day < 1 || day > 31)
+        return 0;
     return 1;
 }
 
-void input_parse(std::string input)
+static int value_check(double value)
+{
+    if (value < 0)
+        return (std::cout << "Error: negative value", 0);
+    return 1;
+}
+
+void findnshow(const std::map<std::string, double>& prices, std::string date, double price)
+{
+    if (prices.find(date) == prices.end())
+        
+}
+
+static int input_check(std::string input, const std::map<std::string, double>& prices)
 {
     std::ifstream file(input.c_str());
     
     if (!file.is_open()) 
     {
         std::cerr << "Error: Cannot open " << input << "\n";
-        return;
+        return 0;
     }
     
     std::string line;
@@ -50,21 +55,17 @@ void input_parse(std::string input)
             std::cout << "Error: bad input missing separator" << std::endl;
         std::string date = line.substr(0, pos);
         double value = std::atof(line.substr(pos + 1).c_str());
-        
         if (!date_check(date))
         {
             std::cout << "Error: bad input => " << date << std::endl;
-            return;
+            return 0;
         }
         if (!value_check(value))
-            return;
-        
+            return 0;
+        findnshow(prices, date, value);
     }
     file.close();
+    return 1;
 }
 
-void findnshow(std::map<std::string, double> prices, std::string date, double price)
-{
-    if (prices.at())
-}
 
